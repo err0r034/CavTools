5812fe41180891196a8d61a002fb61d676ceb37a
Adam Jarvis <adam@jarv.ski>
2016-08-21 16:01:43 +0100

    Groundwork for meeting tracker

---
+++ library/CavTools/ControllerPublic/MeetingGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+class CavTools_ControllerPublic_MeetingGenerator extends XenForo_ControllerPublic_Abstract
+{
+    public function actionIndex()
+    {
+        // Create form for meetings
+    }
+
+    public function actionPost()
+    {
+        // Create meeting
+    }
+}
--- library/CavTools/ControllerPublic/MeetingTracker.php
+++ library/CavTools/ControllerPublic/MeetingTracker.php
@@ -1,1 +1,14 @@
 <?php
+
+class CavTools_ControllerPublic_MeetingTracker extends XenForo_ControllerPublic_Abstract
+{
+    public function actionIndex()
+    {
+        // Get current meetings
+    }
+
+    public function actionPost()
+    {
+        // Remove meeting if needed
+    }
+}
